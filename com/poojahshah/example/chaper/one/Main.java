3de0d31be3a245cac7183366dda1b632f0b19a33
pjs2005 <pjs2005@users.noreply.github.com>
2026-06-09 17:13:38 +0100

    update

--- com/poojahshah/example/chaper/one/Main.java
+++ com/poojahshah/example/chaper/one/Main.java
@@ -1,4 +1,24 @@
 package com.poojahshah.example.chaper.one;
 
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
 public class Main {
+
+
+
+    public static void main(String[] args) {
+        Predicate<String> largerThan5 = s -> s.length() > 5;
+        boolean result = largerThan5.test("hello");
+        System.out.println(result);
+
+        Consumer<String> printUpperCase = s -> System.out.println(s.toUpperCase());
+        printUpperCase.accept("hello from functional programming");
+
+        doSomthing(largerThan5);
+    }
+
+    public static void doSomthing(Predicate<String> p){
+        System.out.println(p.test("doSomehting"));
+    }
 }
