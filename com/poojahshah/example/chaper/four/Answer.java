414b1de73f359f44152ac8b9bd08db72123bf5b9
pjs2005 <pjs2005@users.noreply.github.com>
2026-06-10 11:55:50 +0100

    working

--- com/poojahshah/example/chaper/four/Answer.java
+++ com/poojahshah/example/chaper/four/Answer.java
@@ -21,22 +21,57 @@ class Answer {
         // You can edit this code to try different testing cases.
         List<Integer> numbers = Arrays.asList(5, 10, 15, 20, 25, 30);
         Map<Integer, BigInteger> result = Answer.findAnswer(numbers);
+        System.out.println(result);
+        result.forEach((integer, bigInteger) -> System.out.println(integer + " " + bigInteger));
     }
 
     static Map<Integer, BigInteger> findAnswer(List<Integer> numbers) {
         // Your code goes here.
-        return null;
+        Map<Integer, BigInteger> result = new HashMap<>(numbers.size());
+        ExecutorService executor = Executors.newFixedThreadPool(numbers.size()/3);
+        for(Integer value: numbers){
+            FactorialTask factorialTask = new FactorialTask(value);
+//            Thread thread = new Thread(factorialTask);
+            Future<Map.Entry<Integer, BigInteger>> future = executor.submit(factorialTask);
+
+            try {
+                Map.Entry<Integer, BigInteger> calc = future.get();
+                result.put(calc.getKey(),calc.getValue());
+
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } catch (ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+
+
+        }
+        return result;
     }
 }
 
 class FactorialTask implements Callable<Map.Entry<Integer, BigInteger>> {
 
     // Some of your code goes here
+    private Integer number;
+
+    FactorialTask(Integer number){
+        this.number = number;
+    }
 
     @Override
     public Map.Entry<Integer, BigInteger> call() throws Exception {
         // Your code goes here.
-        return null;
+        BigInteger res = BigInteger.valueOf(1);
+        for(int i = 1;i <= number; i++){
+//            res *= i;
+            res = res.multiply(BigInteger.valueOf(i));
+
+        }
+
+        Map.Entry<Integer, BigInteger> entry = Map.entry(number,res);
+
+        return entry;
     }
 }
 
