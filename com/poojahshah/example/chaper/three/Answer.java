e47181a0d61131d10c99fce69f0e055d7afed931
pjs2005 <pjs2005@users.noreply.github.com>
2026-06-10 11:03:27 +0100

    corrected

--- com/poojahshah/example/chaper/three/Answer.java
+++ com/poojahshah/example/chaper/three/Answer.java
@@ -1,4 +1,29 @@
 package com.poojahshah.example.chaper.three;
 
-public class Answer {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+class Answer {
+
+    // Change these boolean values to control whether you see
+    // the expected result and/or hints.
+    static boolean showExpectedResult = false;
+    static boolean showHints = false;
+
+    // Add annotation to this method
+    @MethodDescription("findAnswer")
+    public static void findAnswer() {
+
+    }
+
+}
+
+// The implementation of the annotation goes here
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@interface MethodDescription {
+    String value();
+
 }
