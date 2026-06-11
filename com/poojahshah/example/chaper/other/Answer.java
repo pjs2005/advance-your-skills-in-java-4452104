83eb15f1b18a819577d03f5d5ed6fd3fbaa8bcb0
pjs2005 <pjs2005@users.noreply.github.com>
2026-06-11 14:51:41 +0100

    fixed

--- com/poojahshah/example/chaper/other/Answer.java
+++ com/poojahshah/example/chaper/other/Answer.java
@@ -1,7 +1,13 @@
 package com.poojahshah.example.chaper.other;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 class Answer {
 
@@ -11,35 +17,40 @@ class Answer {
     static boolean showHints = false;
 
     public static void main(String[] args) {
-        String result = Answer.getRootExceptionMessage();
+        String result = Answer.getAllExceptions();
         System.out.println(result);
     }
 
-    // Return the largest number in the 'numbers' array
-    static String getRootExceptionMessage() {
+    // Return all the messages for all exceptions thrown
+    static String getAllExceptions() {
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        List<String> exceptions = new ArrayList<>();
         try {
-            methodA();
-        } catch (Exception e) {
-            return "Exception: " + e.getMessage();
+            for (int i = 0; i < 10; i++) {
+                try {
+                    Future<?> result = executorService.submit(Answer::getTask);
+                    result.get();
+                    System.out.println("test");
+                } catch (RuntimeException e) {
+                    exceptions.add(e.getMessage());
+                } catch (ExecutionException e) {
+                    exceptions.add(e.getMessage());
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        } finally {
+            executorService.shutdown();
         }
-        return "no exceptions!";
+        return exceptions.toString();
     }
 
-    private static void methodA() {
-        try {
-            methodB();
-        } catch (Exception ignored) {
-            throw ignored;
-        }
-    }
-
-    private static void methodB() {
-        methodC();
-    }
-
-    private static void methodC() {
-        throw new RuntimeException("Exception in methodC");
+    public static void getTask() throws RuntimeException {
+        String threadName = Thread.currentThread().getName();
+        throw new RuntimeException(
+                "Exception in thread: " + threadName);
     }
 
 }
+
 
