687449e9619de816084047db6316d9c2676e0c04
pjs2005 <pjs2005@users.noreply.github.com>
2026-06-11 14:37:21 +0100

    testone

--- com/poojahshah/example/chaper/other/Answer.java
+++ com/poojahshah/example/chaper/other/Answer.java
@@ -1,6 +1,20 @@
 package com.poojahshah.example.chaper.other;
 
-public class Answer {
+import java.util.List;
+import java.util.stream.Collectors;
 
+class Answer {
+
+    // Change these boolean values to control whether you see
+    // the expected result and/or hints.
+    static boolean showExpectedResult = false;
+    static boolean showHints = false;
+
+    // Modfify the list to remove the String in toRemove
+    static String modifyList(List<String> items, String toRemove) {
+
+        return items.stream().filter(item -> !item.equals(toRemove)).collect(Collectors.toList()).toString();
+    }
 
 }
+
